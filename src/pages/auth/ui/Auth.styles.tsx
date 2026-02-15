5c82e6cad2bfd3350628321fa1174039a32c81b7
puzika <tolibjonlatipovk@gmail.com>
2026-02-15 19:59:23 +0700

    added logo on auth pages

--- src/pages/auth/ui/Auth.styles.tsx
+++ src/pages/auth/ui/Auth.styles.tsx
@@ -3,13 +3,12 @@ import styled from "styled-components";
 export const Auth = styled.main`
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
-  gap: 3.5rem;
+  gap: 2rem;
   height: 100vh;
   min-width: 25rem;
   max-width: 50rem;
   width: 50%;
-  padding: 2rem;
+  padding: 5rem 0;
   margin: 0 auto;
 `;
--- src/pages/auth/ui/Auth.tsx
+++ src/pages/auth/ui/Auth.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
+import { Logo } from '../../../shared/ui/logo/Logo';
 import * as S from './Auth.styles';
 
 export const AuthPage = () => {
   return (
     <S.Auth>
+      <Logo />
       <Outlet />
     </S.Auth>
   )

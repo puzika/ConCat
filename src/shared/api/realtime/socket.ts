de3f5215f68bdb1a706db7f8180f37f0bfeed562
puzika <tolibjonlatipovk@gmail.com>
2026-07-11 17:49:45 +0700

    refactor: sepearte socket and server urls

--- src/shared/api/realtime/socket.ts
+++ src/shared/api/realtime/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = import.meta.env.VITE_SERVER_URL;
+const URL = import.meta.env.VITE_SOCKET_URL;
 
 export const socket = io(URL, {
   autoConnect: false,
